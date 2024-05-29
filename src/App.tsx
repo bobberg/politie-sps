7a5517aa494b67ffa1a11271fbe72b1e1e8b2e0f
bobberg <bobberg90@hotmail.com>
2024-05-29 17:54:11 +0200

    changed url for hacky solution video calling

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,98 @@
-import React from "react";
+import React, { useState, useEffect, useMemo } from "react";
+import { PublicClientApplication } from "@azure/msal-browser";
+import { Client } from "@microsoft/microsoft-graph-client";
 import "./App.css";
 import PolitieLogo from "./assets/PolitieLogo";
-import { EmergencyPhone } from "./assets/EmergencyPhone";
-
-// Import your icons here. For example:
+// Import your icons here
 import Info from "./assets/Info";
 import Notebook from "./assets/Notebook";
 import VideoCamera from "./assets/VideoCamera";
 import Zien from "./assets/Zien";
+import { EmergencyPhone } from "./assets/EmergencyPhone";
+
+// import {
+//   AzureCommunicationTokenCredential,
+//   CommunicationUserIdentifier,
+// } from "@azure/communication-common";
+// import {
+//   CallComposite,
+//   fromFlatCommunicationIdentifier,
+//   useAzureCommunicationCallAdapter,
+// } from "@azure/communication-react";
+
+// const msalConfig = {
+//   auth: {
+//     clientId: "YOUR_CLIENT_ID",
+//     authority: "https://login.microsoftonline.com/YOUR_TENANT_ID",
+//     redirectUri: "http://localhost:3000",
+//   },
+// };
+
+// const app = new PublicClientApplication(msalConfig);
 
 const App = () => {
+  //   // Replace '' with the ACS user identity value
+  //   const [userId, setUserId] = useState<string>("");
+
+  //   // Replace '' with the ACS token value
+  //   const [token, setToken] = useState<string>("");
+
+  //   // Replace '' with the Teams meeting link value
+  //   const [teamsMeetingLink, setTeamsMeetingLink] = useState<string>("");
+
+  //   const [accessToken, setAccessToken] = useState(null);
+
+  //   const credential = useMemo(() => {
+  //     if (token) {
+  //       return new AzureCommunicationTokenCredential(token);
+  //     }
+  //     return;
+  //   }, [token]);
+
+  //   const callAdapterArgs = useMemo(() => {
+  //     if (userId && credential && displayName && teamsMeetingLink) {
+  //       return {
+  //         userId: fromFlatCommunicationIdentifier(
+  //           userId
+  //         ) as CommunicationUserIdentifier,
+  //         displayName,
+  //         credential,
+  //         locator: { meetingLink: teamsMeetingLink },
+  //       };
+  //     }
+  //     return {};
+  //   }, [userId, credential, displayName, teamsMeetingLink]);
+
+  //   const callAdapter = useAzureCommunicationCallAdapter(callAdapterArgs);
+
+  //   useEffect(() => {
+  //     /* Commenting out for now
+
+  //     const init = async () => {
+  //         setMessage('Getting ACS user');
+  //         //Call Azure Function to get the ACS user identity and token
+  //         let res = await fetch(process.env.REACT_APP_ACS_USER_FUNCTION as string);
+  //         let user = await res.json();
+  //         setUserId(user.userId);
+  //         setToken(user.token);
+
+  //         setMessage('Getting Teams meeting link...');
+  //         //Call Azure Function to get the meeting link
+  //         res = await fetch(process.env.REACT_APP_TEAMS_MEETING_FUNCTION as string);
+  //         let link = await res.text();
+  //         setTeamsMeetingLink(link);
+  //         setMessage('');
+  //         console.log('Teams meeting link', link);
+  //     }
+  //     init();
+
+  //     */
+  //   }, []);
+
   return (
     <div className="App">
       <nav className="navbar" style={{ backgroundColor: "#182866" }}>
-        {/* Replace src with the path to the Politie logo */}
         <PolitieLogo />
-        {/* <img
-          src="politie-logo.png"
-          alt="Politie Logo"
-          className="navbar-logo"
-        /> */}
       </nav>
       <main className="main-body" style={{ backgroundColor: "#EAF0F7" }}>
         <h1>Hoe kunnen we je helpen?</h1>
@@ -46,18 +119,26 @@ const App = () => {
             </div>
           </a>
 
-          <a href="https://www.politie.nl/">
+          <a href="https://politie-sps.azurewebsites.net/visit">
             <div className="button">
               <VideoCamera />
-              <span>Ik wil direct contact met een politieagent</span>
+              <span>Start Teams Video Call</span>
             </div>
           </a>
+          {/* {callAdapter ? (
+            <div>
+              <h1>Contact Customer Service</h1>
+              <div className="wrapper">
+                <CallComposite adapter={callAdapter} />
+              </div>
+            </div>
+          ) : (
+            <></>
+          )} */}
         </div>
       </main>
       <footer className="footer">
         <EmergencyPhone />
-        {/* <EmergencyPhone alt="112 logo" className="footer-logo" /> */}
-        {/* <img src={EmergencyPhone} alt="112 Logo" className="footer-logo" /> */}
         <span>In noodgevallen, bel 112</span>
       </footer>
     </div>
