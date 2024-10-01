47039af2fc0e6b8f68795e78c9eb1b38f712a8de
bobberg <bobberg90@hotmail.com>
2024-10-01 18:37:19 +0200

    fix for website digital human page not loading

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 import Home from "./pages/Home";
-import DigitalHumanPage from "./pages/PallyPage";
+import DigitalHumanPage from "./pages/DigitalHumanPage";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/digital-human" element={<DigitalHumanPage />} /> */}
+        <Route path="/digital-human" element={<DigitalHumanPage />} />
+        {/* I am getting 404 for /digital-human. How to fix */}
+
         {/* Add other routes if needed */}
       </Routes>
     </Router>
